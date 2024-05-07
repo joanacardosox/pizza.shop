6ffa732f2d0e0de6a877b36a38fb8e1a9dfcf583
Joana Maria <joanamariacardoso1601@gmail.com>
2024-05-07 19:56:29 +0100

    config eslint

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./global.css";
 
 export function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className=" flex h-screen w-10 items-center bg-black justify-center">
       <Button>Enviar</Button>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,11 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
-  prefix: "",
   theme: {
     container: {
       center: true,
@@ -74,4 +73,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
